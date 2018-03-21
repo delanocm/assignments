b2d0cb57c203e21b3fcbb3df6af0a332fcbdefcb
delanoc <delanoc@simmons.edu>
2018-03-20 20:48:31 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,34 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Ben";
+let your_name = "Milly";
+
+if (my_name === "Ben") {
+  renderOutput("That's me!");
+} else if (my_name === "Milly") {
+   renderOutput("Not me!");
+} else {
+  renderOutput("I must be someone else");
+}
+ 
+function helloWorld() {
+  renderOutput("Hello World");
+}
+
+helloWorld();
+
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+helloName("Ben");
+helloName("Loch");
+helloName("Milly");
+
+function todaysHours(hours) {
+  renderOutput(hours);
+}
+
+// todasyHours("Tuesday: 9a -5p");
+
+hours.forEach(todaysHours);
